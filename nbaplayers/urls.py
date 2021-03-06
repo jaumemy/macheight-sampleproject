f37ab038d422b467bca0b0acc07c89ad2a7bba07
jaumemy <jaume.montane@outlook.com>
2021-03-05 19:15:57 -0500

    added player matches and player detail templates

--- nbaplayers/urls.py
+++ nbaplayers/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('player_detail/', views.player_detail, name='player-detail'),
+    path('player_matches/', views.player_matches, name='player-matches'),
     # path('playerlist/', views.PlayerListView, name='playerlist'),
 ]
--- nbaplayers/views.py
+++ nbaplayers/views.py
@@ -16,3 +16,44 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+
+def player_detail(request):
+
+    url = 'https://mach-eight.uc.r.appspot.com/'
+
+    response = requests.get(url).json()
+
+    context = {
+        'response': response,
+    }
+
+    return render(request, 'player_detail.html', context=context)
+
+
+
+def player_matches(request):
+
+    url = 'https://mach-eight.uc.r.appspot.com/'
+
+    response = requests.get(url).json()
+
+    if request.method == 'POST':
+
+        sum = request.POST.get('sum')
+
+        context = {
+            'response': response,
+            'sum': sum
+        }
+
+        return render(request, 'player_matches_result.html', context=context)
+
+
+
+    context = {
+        'response': response,
+    }
+
+    return render(request, 'player_matches.html', context=context)
