a85e57d1434c9506209a8bf70d672500bccd5e90
jaumemy <jaume.montane@outlook.com>
2021-03-05 20:50:46 -0500

    algorithm warking with integers

--- nbaplayers/views.py
+++ nbaplayers/views.py
@@ -43,9 +43,53 @@ def player_matches(request):
 
         sum = request.POST.get('sum')
 
+        def pairs(arr, n, sum):
+
+            dict_ = dict()
+            list_ = []
+
+            for i in range(n):
+
+                temp = sum - arr[i]
+
+                if temp in dict_:
+                    count = dict_[temp]
+
+                    for j in range(count):
+                        x = (temp, arr[i])
+                        list_.append(x)
+
+                if arr[i] in dict_:
+                    dict_[arr[i]] += 1
+
+                else:
+                    dict_[arr[i]] = 1
+
+            return list_
+
+
+        arr = []
+
+        for i in response['values']:
+            h = int(i['h_in'])
+            arr.append(h)
+
+        n = len(arr)
+
+        sum=int(sum)
+
+        result_list = pairs(arr, n, sum)
+
+
+
+
+
+
+
         context = {
             'response': response,
-            'sum': sum
+            'sum': sum,
+            'result_list': result_list,
         }
 
         return render(request, 'player_matches_result.html', context=context)
